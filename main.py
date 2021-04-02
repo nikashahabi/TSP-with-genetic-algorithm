e550c69116b78e2cf3d3af3c52fcb051489bf3af
nika shahabi <nikashahabi@aut.ac.ir>
2021-04-02 19:22:01 +0430

    mutation and crossover methods added

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Individual:
         self.fit = self.computeFitness()
     def computeFitness(self):
         d = 0
-        for i in range(self.size):
+        for i in range(Individual.size):
             start = int(self.string[i])
             end = int(self.string[i+1])
             distance = Individual.map[start][end]
@@ -34,44 +34,104 @@ class Individual:
         string = string + string[0]
         return string
     @staticmethod
-    def reproduce(individual1, individual2):
-        rand = random.randrange(0, )
+    def partiallyMappedCrossover(individual1, individual2):
+        i = random.randrange(0, Individual.size+1)
+        j = random.randrange(0,Individual.size+1)
+        while j == i:
+            j = random.randrange(0, Individual.size+1)
+        if j < i:
+            i, j = j, i
+        child1 = individual1.string[i:j]
+        child2 = individual2.string[i:j]
+        map1to2 = {}
+        map2to1 = {}
+        for k in range(i, j):
+            map1to2[individual1.string[k]] = individual2.string[k]
+            map2to1[individual2.string[k]] = individual1.string[k]
+        'child 1 is now getting complete'
+        child1half = ""
+        for k in range(0, i):
+            temp = individual2.string[k]
+            while temp in child1:
+                temp = map1to2[temp]
+            child1half = child1half + temp
+        child1 = child1half + child1
+        for k in range(j, Individual.size):
+            temp = individual2.string[k]
+            while temp in child1:
+                temp = map1to2[temp]
+            child1 = child1 + temp
+        child2half = ""
+        for k in range(0, i):
+            temp = individual1.string[k]
+            while temp in child2:
+                temp = map2to1[temp]
+            child2half = child2half + temp
+        child2 = child2half + child2
+        for k in range(j, Individual.size):
+            temp = individual1.string[k]
+            while temp in child2:
+                temp = map2to1[temp]
+            child2 = child2 + temp
+        child1 = child1 + child1[0]
+        child2 = child2 + child2[0]
+        finalChild = Individual(random.choice([child1, child2]))
+        return finalChild
+
     def muteWithSmallProb(self, prob):
         rand = random.uniform(0,1)
         if rand <= prob:
-            i = random.randrange(0, )
-    'in do ta tabe akhari mundan'
-
+            ij = random.sample(range(0, Individual.size), 2)
+            i = ij[0]
+            j = ij[1]
+            self.string = swapElements(i, j, self.string)
+            if i == 0 or j == 0:
+                 self.string = self.string[0: Individual.size] + self.string[0]
 
 
 class Population:
     size = None
+
     def __init__(self, isEmpty):
         self.individuals = []
         if isEmpty is False:
-            for i in range(Population.populationSize):
+            for i in range(Population.size):
                 self.individuals.append(Individual())
-    def pickRand(self):
-        rand = random.randrange(0, self.populationSize)
-        return self.individuals[rand]
+
+    def pickWithWeights(self):
+        lst = []
+        weights = []
+        for ind in self.individuals:
+            lst.append(ind)
+            weights.append(ind.fit)
+        rands = random.choices(lst, weights, 2)
+        return rands[0], rands[1]
+
     def addIndividual(self, individual):
         self.individuals.append(individual)
+
     def bestIndividual(self):
         best = float("inf")
         for individual in self.individuals:
             if individual.fit <= best:
                 bestInd = individual
-                best = individual.Fit
+                best = individual.fit
         return bestInd
+
     def isFit(self, fitEnough):
         for individual in self.individuals:
             if individual.fit <= fitEnough:
                 return True
         return False
 
+def swapElements(i, j, string):
+    strlist = list(string)
+    strlist[i], strlist[j] = strlist[j], strlist[i]
+    stringnew = "".join(strlist)
+    return stringnew
 
 
-def geneticAlgorithm(map, initialPopulation = None, iterations = 100, fitEnough = None, populationSize = 4, muteProb= 0.1):
+def geneticAlgorithm(map, initialPopulation = None, iterations = 100, fitEnough = 20, populationSize = 4, muteProb= 0.1):
     i = 0
     individualSize = len(map)
     Population.size = populationSize
@@ -81,20 +141,18 @@ def geneticAlgorithm(map, initialPopulation = None, iterations = 100, fitEnough 
         initialPopulation = Population(False)
     currentPopulation = initialPopulation
     while i < iterations and initialPopulation.isFit(fitEnough) is False:
+        print("generic algorithm ", str(i) + "th iteration" )
         newPopulation = Population(True)
         for i in range(populationSize):
-            x = currentPopulation.pickRand()
-            y = currentPopulation.pickRand()
-            child = Individual.reproduce(x, y)
+            x, y = currentPopulation.pickWithWeights()
+            child = Individual.partiallyMappedCrossover(x, y)
             child.muteWithSmallProb(muteProb)
             newPopulation.addIndividual(child)
         currentPopulation = newPopulation
     return(currentPopulation.bestIndividual())
 
 
-
-
-
-
-
-
+map = [[0, 2, 3],
+       [2,0, 0],
+       [100,1,0]]
+geneticAlgorithm(map=map)
